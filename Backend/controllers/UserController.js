7fb5bb4b7960bc0454fde8d19e82362c8294be15
sanketdakhare <sanket.dakhare5@gmail.com>
2024-08-30 00:57:23 +0530

    Update

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -128,7 +128,7 @@ export const login = async (req, res) => {
     const tokenData = {
       userId: user._id,
     };
-    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+    const token =  jwt.sign(tokenData, process.env.SECRET_KEY, {
       expiresIn: "1d",
     });
 
@@ -146,7 +146,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "None",
+        secure:true,
+        sameSite: "None"
       })
       .json({
         message: `Welcome ${user.fullname}`,

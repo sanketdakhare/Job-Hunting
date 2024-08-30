79bc6ee215f343e3f407f0a50ff6dd7bcd5bc364
sanketdakhare <sanket.dakhare5@gmail.com>
2024-08-30 19:54:54 +0530

    Add vercel.json configuration file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,23 +1,23 @@
-// import path from "path"
-// import react from "@vitejs/plugin-react"
-// import { defineConfig } from "vite"
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
  
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'frontend/dist'
-  }
-});
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'frontend/dist'
+//   }
+// });

bd943eef1c50e7737960f2c438812f227a446c84
sanketdakhare <sanket.dakhare5@gmail.com>
2024-08-31 00:06:58 +0530

    changes in LogIn and Signup for link

--- Backend/config/sendEmail.js
+++ Backend/config/sendEmail.js
@@ -18,7 +18,7 @@ const sendEmail = async (email, subject, text, url) => {
             from: process.env.USER,
             to: email,
             subject: subject,
-            html:`<h2>${text}</h2> <a>${url}</a>`
+            html:`<h4>${text}</h4> <a href="${url}" > </a>`
         });
         
         

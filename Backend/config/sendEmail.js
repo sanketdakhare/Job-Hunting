9bf882ea6c5f8909e72cab8695df5ee8a096ea16
sanketdakhare <sanket.dakhare5@gmail.com>
2024-08-31 09:35:57 +0530

    change

--- Backend/config/sendEmail.js
+++ Backend/config/sendEmail.js
@@ -20,7 +20,7 @@ const sendEmail = async (email, subject, url) => {
             subject: subject,
             html :`<h4>Thank you for registering with us!</h4>
                  <p><span><a href="${url}">Click here </a></span>to verify your email address</p>
-                 <p>If you did not request this verification, please ignore this email.</p> ` 
+                 <p>If you did not request this verification mail, please ignore this mail.</p> ` 
         });
         
         

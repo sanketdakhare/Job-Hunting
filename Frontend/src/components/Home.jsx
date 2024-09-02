65048ca3bb59d528aac5e2329857ac86c068cc5e
sanketdakhare <sanket.dakhare5@gmail.com>
2024-09-02 23:15:27 +0530

    use location to call the useGetAllJobs in Home.jsx

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -10,7 +10,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 const Home = () => {
 
-  useGetAllJobs();
+  //useGetAllJobs();
   const { user } = useSelector((store) => store.auth);
   const navigate = useNavigate();
   const location = useLocation();
